1ebc8e33da57ca5c4894ae20b4a22fc552fd68c5
Alex Finnarn <alex.finnarn@gmail.com>
2020-12-22 17:45:47 -0500

    my own custom test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,15 +1,33 @@
-describe('My First Test', () => {
-  it('Gets, types and asserts', () => {
-    cy.visit('https://example.cypress.io')
+describe('Test CU\'s Homepage', () => {
 
-    cy.contains('type').click()
+  beforeEach(() => {
+    // Given I am on the CU homepage.
+    cy.visit('/');
+  });
 
-    // Should be on a new URL which includes '/commands/actions'
-    cy.url().should('include', '/commands/actions')
-
-    // Get an input, type into it and verify that the value has been updated
-    cy.get('.action-email')
-      .type('fake@email.com')
-      .should('have.value', 'fake@email.com')
+  it('Finds logo in header', () => {
+    // When I inspect the header.
+    cy.get('header').within((header) => {
+      // Then I should see the logo.
+      cy.get('#logo').should('be.visible');
+    })
   })
+
+  it('Can interact with search', () => {
+      // When I open the search input.
+      cy.get('.l-region--branding i.fa-search').click();
+
+      // Then I should see search prompt text.
+      cy.contains('Search CU.EDU');
+
+      // And I should see search placeholder text.
+      cy.get('input[name="search"]').should('have.attr', 'placeholder', 'Search Sites & People')
+
+      // When I search for "donor".
+      .type('donor{enter}');
+
+      // The the URL should have the search paramter in it.
+      cy.location('search').should('contains', 'search=donor');
+
+  });
 })
